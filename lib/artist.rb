44e2681cb9ac84e3ef09ca8ed987b85bfbba992d
Elle Faraday <faraday.elle@gmail.com>
2017-11-12 18:30:16 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -15,8 +15,8 @@ class Artist
     @@song_count += 1
   end
 
-  def add_song_by_name(title)
-    song = Song.new(title)
+  def add_song_by_name(name)
+    song = Song.new(name)
     add_song(song)
   end
 
